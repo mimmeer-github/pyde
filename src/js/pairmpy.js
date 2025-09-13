d7c8ed3cd8674cbcbf2ade0e7ad1d609e4650499
mimmeer <175632698+mimmeer-github@users.noreply.github.com>
2025-09-13 06:56:18 +0100

    Update pairmpy.js

--- src/js/pairmpy.js
+++ src/js/pairmpy.js
@@ -1,14 +1,21 @@
 let mybutton = document.getElementById("button")
 
-mybutton.addEventListener("click", async () => {
-  let usbDevOpt = {
-    "filters": [
-      {
-        "vendorId": 0x2e8a,
-        "productId": 0x000f
-      }
-    ]
-  };
+mybutton.addEventListener("click", async () => 
+  try {
+    let usbDevOpt = {
+      "filters": [
+        {
+          "vendorId": 0x2e8a,
+          "productId": 0x000f
+        }
+      ]
+    };
+    
+    let microCont = await navigator.usb.requestDevice(usbDevOpt);
 
-  let microCont = await navigator.usb.requestDevice(usbDevOpt);
+    console.log(microCont);
+  }
+  catch {
+    console.log("Oops! Something went wrong, did you close the dialogue - or was it blocked?");
+  }
 });
