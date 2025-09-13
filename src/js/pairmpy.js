cadb39c9424098122fbead1cbe77fb3072648a85
mimmeer <175632698+mimmeer-github@users.noreply.github.com>
2025-09-13 08:34:52 +0100

    Update pairmpy.js

--- src/js/pairmpy.js
+++ src/js/pairmpy.js
@@ -1,20 +1,39 @@
-let mybutton = document.getElementById("button")
+let mybutton = document.getElementById("button");
+let mc = null;
+
+async function loadMCData() {
+  let resp = await fetch("../../static/data/microcontrollers.json");
+  if (!resp.ok) throw new Error(`microcontrollers.json couldn't be fetched with HTTP error: ${resp.status}`);
+  let data = resp.json();
+  return data;
+}
+
+async function getUsb() {
+  let mcs = (await loadMCData()).microCont
+  let options = {
+    "filters": []
+  }
+  mcs.forEach((mc) => {
+    options.filters.push({
+      "vendorId": mc.usb_id_flash.vid,
+      "productId": mc.usb_id_flash.pid
+    });
+  });
+  try {
+    let microCont = await navigator.usb.requestDevice(options);
+    return microCont;
+  }
+  catch (err) {
+    console.error(`We have a strange problem. ${err}`);
+    return null;
+  }
+}
 
 mybutton.addEventListener("click", async () => 
   {
     try {
-      let usbDevOpt = {
-        "filters": [
-          {
-            "vendorId": 0x2e8a,
-            "productId": 0x000f
-          }
-        ]
-      };
-    
-      let microCont = await navigator.usb.requestDevice(usbDevOpt);
-
-      console.log(microCont);
+      mc = await getUsb();
+      console.log(mc);
     }
     catch {
       console.log("Oops! Something went wrong, did you close the dialogue - or was it blocked?");
